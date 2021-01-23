c00544bdcf487c3b5bc5a040879777d471cb3950
EC2 Default User <ec2-user@ip-172-31-13-112.us-east-2.compute.internal>
2021-01-23 16:01:28 +0000

    レイアウトファイル作成

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,14 @@
+<header class="navber navber-expand-lg navber-light bg-light fixed-top">
+    <a href="/" class="navbar-brand">TODO App</a>
+    <button
+       class="navbar-toggler"
+       type="button"
+       date-toggle="collapse"
+       data-target="#navbarSupportedContent"
+       aria-controls="navbarSuppprtedContent"
+       aria-expanded="false"
+       aria-label="Toggle navigation"
+    >
+       <span class="navbar-toggler-icon"></span>
+    </button>
+</header>
