8312210a27923e7a62ce514854746df88f32125c
EC2 Default User <ec2-user@ip-172-31-13-112.us-east-2.compute.internal>
2021-01-24 13:40:59 +0000

    chenge login layouts

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -11,4 +11,24 @@
     >
        <span class="navbar-toggler-icon"></span>
     </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ml-auto">
+            @auth
+            <li class="navbar-item">
+                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementVyId('logout-form').submit();">{{_('logout') }}</a>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    @csrf
+            </li>
+            @else
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('register') }}">{{ _('Register') }}</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('login') }}">{{ _('Login') }}</a>
+            </li>
+            @endauth
+            
+            
+        </ul>
+    </div>
 </header>
